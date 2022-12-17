48151ae30e24c7e53e773bea99ccac6046d05c95
Blaz1c <32022290+BigBlaze7@users.noreply.github.com>
2022-12-17 17:01:28 -0600

    copter changes

--- Assets/Scripts/CopterScript.cs
+++ Assets/Scripts/CopterScript.cs
@@ -6,25 +6,54 @@ public class CopterScript : MonoBehaviour
 {
     [SerializeField] float force = 1;
     [SerializeField] float defaltUpVolocity = 1;
+    [SerializeField] private float verticalForceDamp = 1;
     private Vector2 netForce;
     private Vector2 upForce;
     Rigidbody2D rb;
+    private bool checkVol = true;
+    private Vector3 newScail;
+
+    private int key;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        newScail = rb.transform.localScale;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            key = 1;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            key = -1;
+        }
+        
         netForce.x = force * Input.GetAxis("Horizontal");
-        netForce.y = force * Input.GetAxis("Vertical");
-        // upForce.y = defaltUpVolocity - netForce.y;
-        // if (upForce.y > 0)
-        // {
-        //     rb.AddForce(upForce);
-        // }
+        netForce.y = (force * Input.GetAxis("Vertical"))/verticalForceDamp;
+        
+        if (key < 0 && !checkVol)
+        {
+            Flip();
+            checkVol = true;
+        }
+        else if (key > 0 && checkVol)
+        {
+            Flip();
+            checkVol = false;
+        }
+        
         rb.AddForce(netForce);
     }
+    void Flip()
+    {
+        newScail.x *= -1;
+        rb.transform.localScale = newScail;
+    }
 }
+
+
