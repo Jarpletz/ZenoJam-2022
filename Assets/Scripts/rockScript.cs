6dbcf2e10ad5065365d7c8f703f68e848c3429b6
Blaz1c <32022290+BigBlaze7@users.noreply.github.com>
2022-12-18 17:13:34 -0600

    rock

--- Assets/Scripts/rockScript.cs
+++ Assets/Scripts/rockScript.cs
@@ -7,17 +7,22 @@ public class rockScript : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] Vector3 copterPoss;
     [SerializeField] private float deadZone = -50;
+    Rigidbody2D rb;
+    private Vector2 netForce;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         copterPoss = GameObject.FindWithTag("Player").transform.position;
-        transform.position = transform.position + (Vector3.down * speed) * Time.deltaTime;
+        netForce[0] = 0;
+        netForce[1] = speed;
+        rb.AddForce(netForce);
+        
         if (transform.position.y < copterPoss.y - 50)
         {
             Destroy(gameObject);
