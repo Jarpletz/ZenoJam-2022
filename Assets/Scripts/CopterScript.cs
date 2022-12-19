1d98da2d590bf79af4f6be779021ab0702e6d420
Blaz1c <32022290+BigBlaze7@users.noreply.github.com>
2022-12-19 14:21:44 -0600

    health

--- Assets/Scripts/CopterScript.cs
+++ Assets/Scripts/CopterScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,18 @@ public class CopterScript : MonoBehaviour
     [SerializeField] private float verticalForceDamp = 1;
     private Vector2 netForce;
     Rigidbody2D rb;
+    Collision coll;
     private bool checkVol = true;
     private Vector3 newScail;
 
-    [SerializeField] private float health = 100;
-
+    [SerializeField] GameManager gameManager;
+    
     private int key;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        gameManager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager>();
         newScail = rb.transform.localScale;
     }
 
@@ -32,9 +35,9 @@ public class CopterScript : MonoBehaviour
         {
             key = -1;
         }
-        
         netForce[0] = force * Input.GetAxis("Horizontal");
         netForce[1] = (force * Input.GetAxis("Vertical"))/verticalForceDamp;
+        
         
         if (key < 0 && !checkVol)
         {
@@ -48,12 +51,39 @@ public class CopterScript : MonoBehaviour
         }
         
         rb.AddForce(netForce);
+        
     }
     void Flip()
     {
         newScail.x *= -1;
         rb.transform.localScale = newScail;
     }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.collider.name == "Falling Rocks(Clone)")
+        {
+            gameManager.health -= 20;
+        }
+        if(col.collider.name == "The Bat")
+        {
+            gameManager.health -= 20;
+        }
+
+        // if (col.collider.name == "Foreground")
+        //{
+        //
+        // }
+    }
+
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.name == "Lava" )
+        {
+            gameManager.health -= 10 * Time.deltaTime;
+        }
+    }
+    
 }
 
 
