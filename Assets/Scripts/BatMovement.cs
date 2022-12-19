dc1141b1bc7954301bd50ae3c402e85d8c4fd947
Blaz1c <32022290+BigBlaze7@users.noreply.github.com>
2022-12-19 14:56:32 -0600

    bat phy and intereaction with copter

--- Assets/Scripts/BatMovement.cs
+++ Assets/Scripts/BatMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class BatMovement : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class BatMovement : MonoBehaviour
    [SerializeField] Transform[] points;
    [SerializeField] float speed;
    [SerializeField] float distanceThreshold;
+   private bool alive = true;
+   GameObject Player;
+   [SerializeField] float slownessMultiplier = 2;
+
+
 
    Vector3 direction;
    Vector3 scaleFlip;
@@ -16,46 +22,64 @@ public class BatMovement : MonoBehaviour
    int currentTarget=0;
 
    Rigidbody2D rb;
+   private Vector2 netForce;
+   private int deadLayer;
+
 
    void Start()
    {
       rb = GetComponent<Rigidbody2D>();
-
+      deadLayer = LayerMask.NameToLayer("Dead");
       transform.position = points[0].position;
+      Player = GameObject.FindWithTag("Player");
+
    }
 
    // Update is called once per frame
    void Update()
    {
-      if (Vector3.Distance(transform.position, points[currentTarget].position) < distanceThreshold)
-      {//If close to the targeted point
-
-         if(currentTarget == 0 && pointDir == -1)
-         {
-            pointDir = 1;
-         }//If has reached the first point, turn around
-         else if(currentTarget == points.Length - 1 && pointDir == 1)
-         {
-            pointDir = -1;
-         }//If has reached the last point, turn around
-
-         currentTarget += pointDir;
-      }
-
-      direction= Vector3.MoveTowards(transform.position, points[currentTarget].position, speed*Time.deltaTime);
-
-      transform.position = direction;
-
-      if (points[currentTarget].position.x > transform.position.x)
+      if (alive)
       {
-         scaleFlip.Set(1, 1, 1);
+         if (Vector3.Distance(transform.position, points[currentTarget].position) < distanceThreshold)
+         {
+            //If close to the targeted point
+
+            if (currentTarget == 0 && pointDir == -1)
+            {
+               pointDir = 1;
+            } //If has reached the first point, turn around
+            else if (currentTarget == points.Length - 1 && pointDir == 1)
+            {
+               pointDir = -1;
+            } //If has reached the last point, turn around
+
+            currentTarget += pointDir;
+         }
+
+         direction = Vector3.MoveTowards(transform.position, points[currentTarget].position, speed * Time.deltaTime);
+
+         transform.position = direction;
+
+         if (points[currentTarget].position.x > transform.position.x)
+         {
+            scaleFlip.Set(1, 1, 1);
+         }
+         else
+         {
+            scaleFlip.Set(-1, 1, 1);
+         }
+
+         transform.localScale = scaleFlip;
       }
       else
       {
-         scaleFlip.Set(-1, 1, 1);
+         
+         netForce[0] = 0;
+         netForce[1] = -2;
+         rb.AddForce(netForce);
+         gameObject.layer = deadLayer;
       }
-      transform.localScale = scaleFlip;
-        
+
    }
 
    private void OnDrawGizmos()
@@ -70,8 +94,17 @@ public class BatMovement : MonoBehaviour
    {
       if (collision.gameObject.CompareTag("Player"))
       {
+         Player.GetComponent<Rigidbody2D>().velocity /= slownessMultiplier;
+         alive = false;
+
          //Do What happens if it hits the player here
       }
+
+      if (collision.gameObject.CompareTag("Flare"))
+      {
+         alive = false;
+      }
    }
+   
 
 }
